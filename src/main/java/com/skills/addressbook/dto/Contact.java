0adc252845752b084dbfacf104c7d59732091b2d
sylvester.musyoki <96wF*VjP2dw^L@Jl1@7M>
2021-09-11 13:15:36 +0300

    :necktie: Add Contact.java POJO

---
+++ src/main/java/com/skills/addressbook/dto/Contact.java
@@ -0,0 +1,17 @@
+package com.skills.addressbook.dto;
+
+import lombok.Data;
+
+/**
+ * Created by Sylvester Musyoki
+ * Project addressbook
+ * User: sylvester.musyoki
+ * Date: 11/09/2021
+ * Time: 12:59
+ */
+@Data
+public class Contact {
+    String id;
+    String name;
+    String email;
+}
