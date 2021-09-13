3b7094c8517d8b7a1703265536e3ff467138df8a
sylvester.musyoki <96wF*VjP2dw^L@Jl1@7M>
2021-09-13 23:46:51 +0300

    :sparkles: Add API Meta Data

--- src/main/java/com/skills/addressbook/configs/SpringFoxConfig.java
+++ src/main/java/com/skills/addressbook/configs/SpringFoxConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 /**
  * Created by Sylvester Musyoki
@@ -22,6 +26,19 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.skills"))
                 .paths(PathSelectors.ant("/api/*"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Address Book API",
+                "Simple API to manage Addresses.",
+                "1.0",
+                "Terms of service",
+                new Contact("Sylvester Musyoki", "www.example.com", "musyokisyl81@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
     }
 }
